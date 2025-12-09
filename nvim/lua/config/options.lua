507e8ea3aa5f0679b8e4885df9f05b3c5b99dc72
Bartosz Stachowiak <sbartekt@proton.me>
2025-12-09 13:17:41 +0100

    update deps

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,5 +1,5 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.relativenumber = false
+vim.opt.relativenumber = true
 vim.g.python3_host_prog = vim.env.NEOVIM_PYTHON_PATH or "python3.13"

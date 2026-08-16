322c137b69a74453ea15cc19a4e3a6a72b51174b
Bartosz Stachowiak <sbartekt@proton.me>
2026-08-16 11:48:08 +0200

    customize omarchy menu

--- hypr/.config/hypr/bindings.lua
+++ hypr/.config/hypr/bindings.lua
@@ -1,3 +1,6 @@
 -- Keep Omarchy's default SUPER bindings, except use Super+tilde for the root menu.
+hl.unbind("SUPER + SPACE")
 hl.unbind("SUPER + GRAVE")
+hl.unbind("SUPER + ALT + SPACE")
 o.bind("SUPER + GRAVE", "Omarchy menu", "omarchy menu")
+o.bind("ALT + GRAVE", "Apps menu", "omarchy-menu toggle apps")
--- omarchy/.plugins/install.sh
+++ omarchy/.plugins/install.sh
@@ -19,3 +19,10 @@ cp -a "${OMARCHY_PATH:-/usr/share/omarchy}/shell/plugins/bar"/. "$bar_dir"/
 cp -a "$plugin_root/barti.bar/manifest.json" "$bar_dir"/
 rm -f "$bar_dir/Bar.qml.orig" "$bar_dir/Bar.qml.rej"
 patch --batch --no-backup-if-mismatch -d "$bar_dir" -p1 < "$plugin_root/barti.bar/Bar.patch"
+
+menu_dir="$target_root/barti.menu"
+mkdir -p "$menu_dir"
+cp -a "${OMARCHY_PATH:-/usr/share/omarchy}/shell/plugins/menu"/. "$menu_dir"/
+cp -a "$plugin_root/barti.menu/manifest.json" "$menu_dir"/
+rm -f "$menu_dir/Menu.qml.orig" "$menu_dir/Menu.qml.rej"
+patch --batch --no-backup-if-mismatch -d "$menu_dir" -p1 < "$plugin_root/barti.menu/Menu.patch"
